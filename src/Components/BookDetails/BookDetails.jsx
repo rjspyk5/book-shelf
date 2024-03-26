84cd8a982c514163bf6f99e21b2845f63b5c3beb
rjspyk5 <rjspyk5@gmail.com>
2024-03-26 17:32:57 +0600

    book details card fifty percentage desgain complete

--- src/Components/BookDetails/BookDetails.jsx
+++ src/Components/BookDetails/BookDetails.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router-dom";
 import { Catagory } from "../Books/Catagory";
-import { CiStar } from "react-icons/ci";
 
 export const BookDetails = () => {
   const { data } = useLoaderData();
@@ -10,25 +9,29 @@ export const BookDetails = () => {
     data.find((el) => el.bookId === parseInt(id));
 
   return (
-    <div className="grid grid-cols-2 gap-10">
-      <div className="flex rounded-md min-w-[500px] min-h-[700px] justify-center bg-[#F3F3F3] items-center">
+    <div className="grid grid-cols-2 gap-10 font-work-sans">
+      <div className="flex rounded-md min-w-[500px]  justify-center bg-[#F3F3F3] items-center">
         <img src="" alt="" className="rounded-md" />
       </div>
       <div>
-        <div className="space-y-4 py-5 border-b flex-grow border-[#13131326] border-dashed">
-          <div className="flex gap-3 items-center">
-            {tags.map((el, idx) => (
-              <Catagory key={idx} tagname={el} />
-            ))}
-          </div>
-          <h1 className="font-bold text-2xl">{bookName}</h1>
-          <p className="font-work-sans font-medium">By : {author}</p>
+        <div className="space-y-4 py-5 border-b flex-grow border-[#13131326]">
+          <h1 className="font-bold text-4xl  font-playfair">{bookName}</h1>
+          <p className=" font-medium text-[#131313CC]">By : {author}</p>
         </div>
-        <div className="pt-4 flex font-work-sans justify-between font-medium items-center">
-          <p>{category}</p>
-          <p className="flex gap-1 items-center">
-            {rating} <CiStar />
-          </p>
+
+        <p className="border-b py-3 text-[#131313CC] border-[#13131326]  font-medium">
+          {category}
+        </p>
+        <p className="py-5 text-[#131313B3]">
+          <span className="font-bold text-[#131313]">Review : </span>
+          {review}
+        </p>
+
+        <div className="flex gap-3 items-center border-[#13131326] border-b pb-5 ">
+          <span className="font-bold">Tag</span>
+          {tags.map((el, idx) => (
+            <Catagory key={idx} tagname={el} />
+          ))}
         </div>
       </div>
     </div>
