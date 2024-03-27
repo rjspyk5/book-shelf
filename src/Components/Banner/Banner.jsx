0dbb2ebc48842d73f74a1057cc0c6017c4f284e0
rjspyk5 <rjspyk5@gmail.com>
2024-03-27 16:40:30 +0600

    home section image sizing complete

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import bannerImg from "../../assets/images/pngwing 1.png";
 
 export const Banner = () => {
   return (
-    <div className="md:px-28 px-10 md:py-20 py-12 bg-[#1313130D] rounded-3xl grid grid-cols-5 gap-20">
-      <div className="col-span-3">
+    <div className="md:px-28 px-10 md:py-20 py-12  bg-[#1313130D] rounded-3xl grid md:grid-cols-5 grid-cols-1 gap-20">
+      <div className="md:col-span-3 order-2 md:order-1 text-center md:text-left">
         <h1 className="md:text-5xl text-2xl font-bold md:leading-[84px]">
           Books to freshen up your bookshelf
         </h1>
@@ -14,6 +15,13 @@ export const Banner = () => {
         >
           View The List
         </Link>
+      </div>
+      <div className="order-1 md:order-2 flex justify-center items-center">
+        <img
+          className=" min-w-[200px] md:min-w-[300px] max-h-[320px] "
+          src={bannerImg}
+          alt=""
+        />
       </div>
     </div>
   );
