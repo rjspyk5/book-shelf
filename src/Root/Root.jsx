edef4e2fe9a515c17b4febc4d903153f9cfbbfeb
rjspyk5 <rjspyk5@gmail.com>
2024-03-28 03:50:39 +0600

    delete footer component

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,12 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../Pages/Header/Header";
-import { Footer } from "../Pages/Footer/Footer";
+
 export const Root = () => {
   return (
     <div className="max-w-[1170px] mx-auto px-5">
       <Header></Header>
       <Outlet></Outlet>
-      <Footer></Footer>
     </div>
   );
 };
