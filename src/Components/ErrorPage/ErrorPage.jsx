d6cabc7a3017fa204c874650830c58e8197ac2b6
rjspyk5 <rjspyk5@gmail.com>
2024-03-28 11:58:25 +0600

    add back button at error page

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,17 +1,23 @@
-import { useRouteError } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 export const ErrorPage = () => {
   const error = useRouteError();
-  console.log(error);
+  const navigate = useNavigate();
   return (
     <div className="h-screen w-full flex justify-center items-center">
       {" "}
-      <div className="font-work-sans font-bold text-center">
+      <div className="font-work-sans text-2xl font-bold text-center">
         <h1>Oops!</h1>
         <p>Sorry, an unexpected error has occurred.</p>
         <p>
           <i>{error.statusText || error.message}</i>
         </p>
+        <button
+          onClick={() => navigate(-1)}
+          className="btn-xs btn sm:btn-sm md:btn-md btn-outline mt-2"
+        >
+          Go Back
+        </button>
       </div>
     </div>
   );
