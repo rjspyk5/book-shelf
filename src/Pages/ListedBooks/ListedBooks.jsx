d9d14d3de4355d98edd6a9991bceb191d228f168
rjspyk5 <rjspyk5@gmail.com>
2024-03-28 15:16:01 +0600

    final revision complete

--- src/Pages/ListedBooks/ListedBooks.jsx
+++ src/Pages/ListedBooks/ListedBooks.jsx
@@ -27,7 +27,7 @@ export const ListedBooks = () => {
           </div>
           <ul
             tabIndex={0}
-            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+            className="dropdown-content z-[1] menu p-2  shadow bg-base-100 rounded-box w-52"
           >
             <li onClick={() => handleSrotingClick("rating")}>
               <a>Rating</a>
